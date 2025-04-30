42ed5eeb6f290a8aead17fa0996b74c303646fe2
NikolaiT1986 <kolyan-04031986@mail.ru>
2025-05-01 00:30:12 +0300

    add logging for transfer

--- src/main/java/org/nikolait/assigment/userdeposit/controller/v1/AccountController.java
+++ src/main/java/org/nikolait/assigment/userdeposit/controller/v1/AccountController.java
@@ -3,6 +3,7 @@ package org.nikolait.assigment.userdeposit.controller.v1;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.nikolait.assigment.userdeposit.dto.AccountResponse;
 import org.nikolait.assigment.userdeposit.dto.TransferRequest;
 import org.nikolait.assigment.userdeposit.mapper.AccountMapper;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user/account")
@@ -28,6 +30,12 @@ public class AccountController {
     @PostMapping("/transfer")
     public void transferFunds(@RequestBody @Valid TransferRequest request) {
         Long fromUserId = SecurityUtils.getCurrentUserId();
-        accountService.transfer(fromUserId, request.getUserId(), request.getValue());
+        try {
+            accountService.transfer(fromUserId, request.getUserId(), request.getValue());
+        } catch (Exception e) {
+            log.error("Executing transfer from user id {} to user id {} for amount {}: {}, {} failed!",
+                    fromUserId, request.getUserId(), request.getValue(), e.getClass().getName(), e.getMessage());
+            throw e;
+        }
     }
 }
