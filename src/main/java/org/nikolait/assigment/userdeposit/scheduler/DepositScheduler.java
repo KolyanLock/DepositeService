5e459fed7eec6567b17cf1a21870b4f6da2153f2
NikolaiT1986 <kolyan-04031986@mail.ru>
2025-05-13 01:24:31 +0300

    feature: add advisory_lock for triggerAccrual

--- src/main/java/org/nikolait/assigment/userdeposit/scheduler/DepositScheduler.java
+++ src/main/java/org/nikolait/assigment/userdeposit/scheduler/DepositScheduler.java
@@ -3,15 +3,19 @@ package org.nikolait.assigment.userdeposit.scheduler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.nikolait.assigment.userdeposit.repository.UserRepository;
+import org.nikolait.assigment.userdeposit.service.AdvisoryLockService;
 import org.nikolait.assigment.userdeposit.service.TransactionService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import static org.nikolait.assigment.userdeposit.emum.AdvisoryLockType.TRIGGER_ACCRUAL;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class DepositScheduler {
 
+    private final AdvisoryLockService lockService;
     private final TransactionService transactionService;
     private final UserRepository userRepository;
 
@@ -24,6 +28,9 @@ public class DepositScheduler {
             initialDelayString = "${deposit.scheduler-init-delay}"
     )
     public void triggerAccrual() {
+        if (!lockService.tryAcquirePermanentLock(TRIGGER_ACCRUAL)) {
+            return;
+        }
         log.info("Starting DepositScheduler triggerAccrual >>>");
         userRepository.findAllIds().forEach(userId -> {
             try {
